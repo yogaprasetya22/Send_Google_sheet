ba38e3821f95ede9d76542c3256e817df3a38694
yogaprasetya22 <yogaagle123z@gmail.com>
2021-03-23 22:05:38 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const scriptURL ="https://script.google.com/macros/s/AKfycbwDY1PhnCuC3FtOPxZhItBEaQpupVdGD9eBKmVItPaS3R291-4ZyodgsuOj5HbUAfBs/exec";
+const form = document.forms["wpu-data"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => form.reset(),
+      Swal.fire({
+        title: document.getElementById("name").value,
+        text: "Terima Kasih",
+        icon: "success",
+      })
+    )
+    .catch((error) => Swal.fire({
+      title: document.getElementById("name").value,
+      text: "Error!!",
+      icon: "error",
+    }));
+});
